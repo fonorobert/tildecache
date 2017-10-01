a0a13e0cc0f36be1328c721834d6e3acaa409fcb
Robert Fono <fonorobert@gmail.com>
2017-10-01 18:17:39 +0200

    revise compass prototype
    cardinal directions are now correct, also implemented special case where
    player == cache

--- src/prot_compass.c
+++ src/prot_compass.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void die(char *msg) {
+	printf("Error: %s\n", msg);
+	exit(1);
+}
 
 int main(int argc, char *argv[]) {
+	if(argc != 5) { 
+		die("Expecting exactly 4 arguments!"); 
+	}
+	// args: playerX, playerY, cacheX, cacheY
 	int player[2] = {atoi(argv[1]), atoi(argv[2])};
 	int cache[2] = {atoi(argv[3]), atoi(argv[4])};
 
+	if (player[0] == cache[0] && player[1] == cache[1]) {
+		printf("here\n");
+		return 0;
+	}
 	if (player[1] < cache[1]) {
 		printf("S");
-	} else {
+	} else if (player[1] > cache[1]) {
 		printf("N");
 	}
 
 	if (player[0] < cache[0]) {
-		printf("E\n");
-	} else {
-		printf("W\n");
+		printf("E");
+	} else if (player[0] > cache[0]) {
+		printf("W");
 	}
+	printf("\n");
 	return 0;
 }
